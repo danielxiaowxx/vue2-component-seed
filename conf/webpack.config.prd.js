f73ec6edb45b5ea82f0d1d8f08be79a60c5a0c72
daniel xiao <daniel xiao>
2016-09-11 19:02:23 +0800

    update

--- conf/webpack.config.prd.js
+++ conf/webpack.config.prd.js
@@ -1,9 +1,14 @@
+var path = require('path');
 var webpack = require('webpack');
 
 var config = require('./config');
 
 // webpack.config.js
 module.exports = {
+
+  entry: {
+    'vue-component-seed': path.join(config.src, 'index.js')
+  },
 
   output: {
     path: config.dist,
---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by danie on 2016/9/11.
+ */
+
+import hello from './hello/index.vue';
+
+module.exports = {
+  hello,
+};
